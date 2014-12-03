24c0a74b8a4f21f81e03222321328538734514b4
Lauren Budorick <lauren@mapbox.com>
2014-12-03 15:57:45 -0800

    Add base curve functionality

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## api
 
-    lineChart(elem, width, [[x, value], ... ], [x, label]);
+    lineChart(elem, width, [[x, value], ... ], baseCurve, [x, label]);
 
 ## install
 
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = canvasLineChart;
 
 //=elem
 
-function canvasLineChart(c, width, data, marker) {
+function canvasLineChart(c, width, data, base, marker) {
     width = width * 2;
     var height = 30 * 2;
     var chartHeight = 20 * 2;
@@ -40,12 +40,42 @@ function canvasLineChart(c, width, data, marker) {
       return ~~(((_ / 20) * (width - margin*2)) + margin);
     }
 
+    function curveMidpoint(a, b) {
+      var mx = a[0] + (b[0]-a[0]) / 2;
+      var t = base === 1 ?
+        (mx - a[0]) / (b[0] - a[0]) :
+        (Math.pow(base, mx - a[0]) - 1) / (Math.pow(base, b[0] - a[0]) - 1);
+      var my = (a[1] * (1 - t)) + (b[1] * t);
+      return [mx, my];
+    }
+
+    function getControlPoints(a, b){
+      // thanks to http://scaledinnovation.com/analytics/splines/aboutSplines.html
+      var c = curveMidpoint(a, b);
+      var x0 = a[0], y0 = a[1], x1 = c[0], y1 = c[1], x2 = b[0], y2 = b[1], t=0.5;
+      var d01 = Math.sqrt(Math.pow(x1 - x0, 2) + Math.pow(y1 - y0, 2));
+      var d12 = Math.sqrt(Math.pow(x2 - x1, 2)+Math.pow(y2 - y1, 2));
+      var fa = t * d01 / (d01 + d12);
+      var fb = t * d12 / (d01 + d12);
+      var p1x = x1 - fa * (x2 - x0);
+      var p1y = y1 - fa * (y2 - y0);
+      var p2x = x1 + fb * (x2 - x0);
+      var p2y = y1 + fb * (y2 - y0);
+      return [[p1x, p1y], [p2x, p2y]];
+    }
+
     // draw the data line
     ctx.strokeStyle = '#999';
     ctx.lineWidth = 4;
 
-    data.forEach(function(data, i) {
-      ctx[i === 0 ? 'moveTo' : 'lineTo'](xScale(data[0]), yScale(data[1]));
+    data.forEach(function(d, i) {
+      if (i === 0) ctx.lineTo(xScale(d[0]), yScale(d[1]));
+      else {
+        var cp = getControlPoints(data[i-1], d);
+        ctx.bezierCurveTo(xScale(cp[0][0]), yScale(cp[0][1]),
+          xScale(cp[1][0]), yScale(cp[1][1]),
+          xScale(d[0]), yScale(d[1]));
+      }
     });
     ctx.stroke();
 
@@ -63,6 +93,7 @@ function canvasLineChart(c, width, data, marker) {
       ctx.fill();
       ctx.stroke();
     });
+
 
     if (marker) {
         var xAnchor = xScale(marker[0]);

639723437ccfdb82ea12a1b33f1f38931da7d2de
Tom MacWright <tom@macwright.org>
2015-09-20 18:06:33 -0400

    Start testing with eslint, begin refactor.
    * [ ] use simple-linear-scale as a scale
    * [ ] use a library for min/max
    * [ ] refactor out more methods
    * [ ] document with JSDoc

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var defined = require('defined');
+
 module.exports = canvasLineChart;
 
 // var elem = document.createElement('canvas');
@@ -6,13 +8,37 @@ module.exports = canvasLineChart;
 
 //=elem
 
+function getControlPoints(a, b) {
+  // thanks to http://scaledinnovation.com/analytics/splines/aboutSplines.html
+  var c = curveMidpoint(a, b);
+  var x0 = a[0], y0 = a[1], x1 = c[0], y1 = c[1], x2 = b[0], y2 = b[1], t=0.5;
+  var d01 = Math.sqrt(Math.pow(x1 - x0, 2) + Math.pow(y1 - y0, 2));
+  var d12 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  var fa = t * d01 / (d01 + d12);
+  var fb = t * d12 / (d01 + d12);
+  var p1x = x1 - fa * (x2 - x0);
+  var p1y = y1 - fa * (y2 - y0);
+  var p2x = x1 + fb * (x2 - x0);
+  var p2y = y1 + fb * (y2 - y0);
+  return [[p1x, p1y], [p2x, p2y]];
+}
+
+function curveMidpoint(a, b) {
+  var mx = a[0] + (b[0] - a[0]) / 2;
+  var t = base === 1 ?
+    (mx - a[0]) / (b[0] - a[0]) :
+    (Math.pow(base, mx - a[0]) - 1) / (Math.pow(base, b[0] - a[0]) - 1);
+  var my = (a[1] * (1 - t)) + (b[1] * t);
+  return [mx, my];
+}
+
 function canvasLineChart(c, height, width, data, base, marker, step, stepSize, min, max, scaleFactor) {
-  var stepSize = stepSize || 1;
-  var min = min || 0;
-  var max = max || 20;
-  var scaleFactor = scaleFactor || 1;
+  stepSize = defined(stepSize, 1);
+  min = defined(min, 0);
+  max = defined(max, 20);
+  scaleFactor = defined(scaleFactor, 1);
   width = width * scaleFactor;
-  var height = height * scaleFactor;
+  height = height * scaleFactor;
   var margin = 12 * scaleFactor;
   var chartHeight = height - (margin * scaleFactor);
   c.width = width;
@@ -50,30 +76,6 @@ function canvasLineChart(c, height, width, data, base, marker, step, stepSize, m
     return ~~(((_ / max) * (width - margin)) + margin/2);
   }
 
-  function curveMidpoint(a, b) {
-    var mx = a[0] + (b[0]-a[0]) / 2;
-    var t = base === 1 ?
-      (mx - a[0]) / (b[0] - a[0]) :
-      (Math.pow(base, mx - a[0]) - 1) / (Math.pow(base, b[0] - a[0]) - 1);
-    var my = (a[1] * (1 - t)) + (b[1] * t);
-    return [mx, my];
-  }
-
-  function getControlPoints(a, b){
-    // thanks to http://scaledinnovation.com/analytics/splines/aboutSplines.html
-    var c = curveMidpoint(a, b);
-    var x0 = a[0], y0 = a[1], x1 = c[0], y1 = c[1], x2 = b[0], y2 = b[1], t=0.5;
-    var d01 = Math.sqrt(Math.pow(x1 - x0, 2) + Math.pow(y1 - y0, 2));
-    var d12 = Math.sqrt(Math.pow(x2 - x1, 2)+Math.pow(y2 - y1, 2));
-    var fa = t * d01 / (d01 + d12);
-    var fb = t * d12 / (d01 + d12);
-    var p1x = x1 - fa * (x2 - x0);
-    var p1y = y1 - fa * (y2 - y0);
-    var p2x = x1 + fb * (x2 - x0);
-    var p2y = y1 + fb * (y2 - y0);
-    return [[p1x, p1y], [p2x, p2y]];
-  }
-
   // draw the data line
   ctx.strokeStyle = '#222';
   ctx.lineWidth = 2 * scaleFactor;
@@ -81,7 +83,7 @@ function canvasLineChart(c, height, width, data, base, marker, step, stepSize, m
   data.forEach(function(d, i) {
     if (i === 0) ctx.lineTo(xScale(d[0]), yScale(d[1]));
     else if (step) {
-      ctx.lineTo(xScale(d[0]), yScale(data[i-1][1]));
+      ctx.lineTo(xScale(d[0]), yScale(data[i - 1][1]));
       ctx.lineTo(xScale(d[0]), yScale(d[1]));
     } else {
       var cp = getControlPoints(data[i-1], d);
@@ -99,7 +101,7 @@ function canvasLineChart(c, height, width, data, base, marker, step, stepSize, m
 
   ctx.fillStyle = '#fff';
   ctx.strokeStyle = '#222';
-  data.forEach(function(data, i) {
+  data.forEach(function(data) {
     // Draw circle
     ctx.beginPath();
     ctx.lineWidth = 2 * scaleFactor;
